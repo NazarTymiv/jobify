f102e69cc57c784b0e34e5282bb10e99697723df
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-05 18:14:41 +0000

    feat: add createUser function in seed file

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,9 +1,45 @@
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient()
 
 async function seed() {
+  const user = await createUser({
+    email: 'email@gmail.com',
+    password: '123123',
+    firstName: 'Nazar',
+    lastName: 'Tymiv'
+  })
+
   process.exit(0)
+}
+
+const createUser = async ({
+  email,
+  password,
+  role = 'EMPLOYEE',
+  firstName,
+  lastName
+}) => {
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const createdUser = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      role,
+      profile: {
+        create: {
+          firstName,
+          lastName
+        }
+      }
+    }
+  })
+
+  console.log('Created User: ', createdUser)
+
+  return createUser
 }
 
 seed().catch(async (e) => {
