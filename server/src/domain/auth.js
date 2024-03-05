c40d1326287c61616b62b4681f632b70cf3a093b
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-05 22:19:55 +0000

    feat: add dbClient file and add request to database register user

--- server/src/domain/auth.js
+++ server/src/domain/auth.js
@@ -1,5 +1,21 @@
+import dbClient from '../utils/dbClient.js'
+
 export default class Auth {
-  static async register(data) {
-    console.log(data)
+  static async register({ email, hashedPassword, role, firstName, lastName }) {
+    const registeredUser = await dbClient.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        role,
+        profile: {
+          create: {
+            firstName,
+            lastName
+          }
+        }
+      }
+    })
+
+    return registeredUser
   }
 }
