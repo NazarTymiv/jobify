e2ff5bfcac1b47b5e8d0ecac1ff2746effd47725
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 01:08:15 +0000

    feat: add update user profile controller

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,5 +1,9 @@
-export const getUserById = async (req, res) => {
+export const getUserById = (req, res) => {
   const { profile } = req.user
 
   res.status(200).json({ profile })
+}
+
+export const updateUserProfile = async (req, res) => {
+  res.status(201).json({ message: 'success' })
 }
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { getUserById } from '../controllers/user.js'
+import { getUserById, updateUserProfile } from '../controllers/user.js'
 import { validateAuthentication } from '../middleware/auth.js'
 
 const router = Router()
 
 router.get('/', validateAuthentication, getUserById)
-router.put('/profile')
+router.put('/profile', updateUserProfile)
 
 export default router
