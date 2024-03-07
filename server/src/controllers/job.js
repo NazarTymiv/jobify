bb476273e817c5f07991b83d31c1a5ebcf41384f
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 21:24:17 +0000

    feat: add functional for creating new job

--- server/src/controllers/job.js
+++ server/src/controllers/job.js
@@ -1,3 +1,10 @@
+import Job from '../domain/job.js'
+
 export const createNewJob = async (req, res) => {
-  res.status(201).json({ message: 'success' })
+  const fields = req.body
+  const { id } = req.user
+
+  const createdJob = await Job.createNewJob(id, fields)
+
+  res.status(201).json({ job: createdJob })
 }
