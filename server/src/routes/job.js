3db7af51a9eb55647fe44389a7cacc034e814c39
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 21:16:38 +0000

    feat: add checkfields error handler

--- server/src/routes/job.js
+++ server/src/routes/job.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { createNewJob } from '../controllers/job.js'
 import { validateAuthentication } from '../middleware/auth.js'
+import { checkFields } from '../middleware/general.js'
 
 const router = Router()
 
-router.post('/', validateAuthentication, createNewJob)
+router.post('/', validateAuthentication, checkFields(['title']), createNewJob)
 
 export default router
