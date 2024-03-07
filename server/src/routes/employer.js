229dbfbd779bede8db349c98ac70dbcf6884d0c8
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 20:21:14 +0000

    feat: add middleware for employer for check employee role

--- server/src/routes/employer.js
+++ server/src/routes/employer.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { getAllEmployers } from '../controllers/employer.js'
 import { validateAuthentication } from '../middleware/auth.js'
+import { checkEmployeeRole } from '../middleware/employer.js'
 
 const router = Router()
 
-router.get('/', validateAuthentication, getAllEmployers)
+router.get('/', validateAuthentication, checkEmployeeRole, getAllEmployers)
 
 export default router
