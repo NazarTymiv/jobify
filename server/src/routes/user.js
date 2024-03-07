9fbc19580dc7a86061080cb897f8e7cda1b9d3f2
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 01:23:59 +0000

    feat: add check fields for update user's profile

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { getUserById, updateUserProfile } from '../controllers/user.js'
 import { validateAuthentication } from '../middleware/auth.js'
+import { checkFields } from '../middleware/user.js'
 
 const router = Router()
 
 router.get('/', validateAuthentication, getUserById)
-router.put('/profile', validateAuthentication, updateUserProfile)
+router.put('/profile', validateAuthentication, checkFields, updateUserProfile)
 
 export default router
