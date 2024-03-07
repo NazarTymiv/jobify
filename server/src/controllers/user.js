e492b09d1479595a9a9c6c9729c32a8787dc1699
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 00:43:46 +0000

    feat: add user controller

---
+++ server/src/controllers/user.js
@@ -0,0 +1,3 @@
+export const getUserById = async (req, res) => {
+  res.status(200).json({ message: 'success' })
+}
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import { getUserById } from '../controllers/user.js'
 
 const router = Router()
+
+router.get('/', getUserById)
 
 export default router
