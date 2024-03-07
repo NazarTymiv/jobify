55c40e6094aa654c00a7b5d57c257558380aeda6
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 20:17:47 +0000

    feat: add controller for get all employers

--- server/src/routes/employer.js
+++ server/src/routes/employer.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import { getAllEmployers } from '../controllers/employer.js'
 
 const router = Router()
+
+router.get('/', getAllEmployers)
 
 export default router
