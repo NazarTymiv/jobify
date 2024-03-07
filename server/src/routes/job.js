050239a3a6e8ed87b81316d9cc1e887f4d3c881e
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-07 21:19:33 +0000

    feat: add checkEmployerRole middleware error handler

--- server/src/routes/job.js
+++ server/src/routes/job.js
@@ -2,9 +2,16 @@ import { Router } from 'express'
 import { createNewJob } from '../controllers/job.js'
 import { validateAuthentication } from '../middleware/auth.js'
 import { checkFields } from '../middleware/general.js'
+import { checkEmployerRole } from '../middleware/employer.js'
 
 const router = Router()
 
-router.post('/', validateAuthentication, checkFields(['title']), createNewJob)
+router.post(
+  '/',
+  validateAuthentication,
+  checkEmployerRole,
+  checkFields(['title']),
+  createNewJob
+)
 
 export default router
