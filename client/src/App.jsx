4906973e16eb6b8c4a6e7ed85459b83b74e8fb40
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-10 23:38:35 +0000

    feat: add colors and fonts to tailwind configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <main className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-5xl text-sky-600 underline hover:text-sky-900 cursor-pointer">
-        Hello
-      </h1>
+      <h1 className="text-5xl text-accent">Jobify</h1>
     </main>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        accent: '#256CFE',
+        black: '#111111',
+        darkGray: '#17191F',
+        grey: '#929292',
+        lightGrey: '#F6F9FF',
+        white: '#FAFAFA'
+      }
+    }
   },
   plugins: []
 }
