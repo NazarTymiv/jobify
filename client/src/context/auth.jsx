dc4b340a36ae68286b176e40e5c0c62be5a5f75f
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-11 15:59:29 +0000

    feat: install axios

--- client/src/context/auth.jsx
+++ client/src/context/auth.jsx
@@ -3,8 +3,8 @@ import { createContext, useContext, useState } from 'react'
 const AuthContext = createContext()
 export const AuthData = () => useContext(AuthContext)
 
-export const AuthWrapper = async () => {
-  const [user, setUser] = useState()
+export const AuthWrapper = () => {
+  const [user, setUser] = useState({})
 
   return <h1>Auth Wrapper</h1>
 }
