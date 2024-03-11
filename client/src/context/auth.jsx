994d802d82a61ac068e781914ae35d3ed6ba62a9
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-11 16:35:02 +0000

    feat: add login function in auth context

--- client/src/context/auth.jsx
+++ client/src/context/auth.jsx
@@ -1,10 +1,21 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { loginUser } from '../services/apiClient'
 
 const AuthContext = createContext()
 export const AuthData = () => useContext(AuthContext)
 
 export const AuthWrapper = () => {
   const [user, setUser] = useState({})
+
+  const login = async (email, password) => {
+    loginUser({ email, password })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    login('email@gmail.com', '123123123')
+  }, [])
 
   return <h1>Auth Wrapper</h1>
 }
