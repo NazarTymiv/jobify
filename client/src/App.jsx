878cbb8d3eded270339ad186c236bb7561213d31
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-12 19:16:03 +0200

    feat: add register function to api client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <main className="w-full h-screen flex justify-center items-center">
       <h1 className="text-5xl text-accent">Jobify</h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
