17626f47ca126b71a6e4679ecd750a81cfa58df3
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-13 00:28:35 +0000

    feat: add home page and test protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './context/auth'
+import { AuthProvider, ProtectedRoute } from './context/auth'
 import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <main className="bg-black">
+    <main className="bg-black min-w-full min-h-screen">
       <AuthProvider>
         <Routes>
           <Route path="login" element={<LoginPage />} />
+
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
     </main>
