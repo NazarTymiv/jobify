4da0f82ae629796f49d4ea36fb50f881c9b55124
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-14 23:14:26 +0000

    feat: add created jobs page

---
+++ client/src/components/CreatedJobItem/index.jsx
@@ -0,0 +1,55 @@
+import { RxCross1 } from 'react-icons/rx'
+import useAuth from '../../hooks/useAuth'
+import { deleteJobFromSaved } from '../../services/apiClient'
+
+const CreatedJobItem = ({ data, getCreatedJobs }) => {
+  const { setMessage } = useAuth()
+
+  // const deleteSavedJob = async () => {
+  //   try {
+  //     await deleteJobFromSaved(data.id)
+
+  //     getSavedJobs()
+  //   } catch (error) {
+  //     setMessage(error.response.data.error)
+  //   }
+  // }
+
+  return (
+    <div className="w-full py-10 px-5 bg-white rounded-xl flex relative">
+      <div className="min-w-[60px] size-[60px] bg-grey rounded-full mr-7"></div>
+
+      <div className="flex flex-col w-full">
+        <h2 className="text-black font-semibold text-2xl mb-5">{data.title}</h2>
+
+        <div className="w-full grid grid-cols-4 gap-10">
+          <div className="flex flex-col">
+            <h3 className="text-black font-semibold text-lg">Description</h3>
+            <p className="text-black text-sm">{data.description}</p>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="text-black font-semibold text-lg">Location</h3>
+            <p className="text-black text-sm">{data.location}</p>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="text-black font-semibold text-lg">Skills</h3>
+            <p className="text-black text-sm">{data.skills}</p>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="text-black font-semibold text-lg">Tags</h3>
+            <p className="text-black text-sm">{data.tags}</p>
+          </div>
+        </div>
+      </div>
+
+      <button className="absolute right-10" onClick={deleteSavedJob}>
+        <RxCross1 className="size-6 text-black font-semibold" />
+      </button>
+    </div>
+  )
+}
+
+export default CreatedJobItem
--- client/src/pages/CreatedJobsPage/index.jsx
+++ client/src/pages/CreatedJobsPage/index.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react'
+import useAuth from '../../hooks/useAuth'
+import Loader from '../../components/Loader'
+import CreatedJobItem from '../../components/CreatedJobItem'
+
 const CreatedJobsPage = () => {
-  return <div>CreatedJobsPage</div>
+  const { setMessage } = useAuth()
+
+  const [jobs, setJobs] = useState()
+
+  const getCreatedJobs = async () => {
+    try {
+    } catch (error) {
+      setMessage(error.response.data.error)
+    }
+  }
+
+  useEffect(() => {
+    getCreatedJobs()
+  }, [])
+
+  return (
+    <div className="w-full h-auto px-52 relative flex flex-col space-y-10">
+      {jobs && jobs.length === 0 ? (
+        <p className="text-white font-semibold self-center text-2xl">
+          You do not have created jobs :(
+        </p>
+      ) : jobs ? (
+        jobs.map((job, index) => (
+          <CreatedJobItem
+            data={job}
+            key={index}
+            getCreatedJobs={getCreatedJobs}
+          />
+        ))
+      ) : (
+        <Loader />
+      )}
+    </div>
+  )
 }
 
 export default CreatedJobsPage
