a0573966a36a3b798ef19e0dea42fe18edca49f0
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-14 16:35:12 +0000

    feat: add home page to navigation

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth'
+import { IoHomeSharp } from 'react-icons/io5'
 
 const Navigation = () => {
   const { user } = useAuth()
@@ -7,7 +8,9 @@ const Navigation = () => {
   return (
     <div className="w-full fixed top-0 py-10 px-14 flex items-center justify-between z-50">
       <div className="">
-        <Link to="/"></Link>
+        <Link to="/">
+          <IoHomeSharp className="size-[40px] text-grey hover:text-white transition-colors" />
+        </Link>
       </div>
       <Link to="/profile">
         <div className="size-[50px] rounded-full bg-grey"></div>
